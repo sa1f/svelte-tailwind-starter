a4467f57043640e37b6e63c6764e558db23d1542
Saif Sajid <saifmsajid@gmail.com>
2020-01-03 00:24:31 -0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+const production = !process.env.ROLLUP_WATCH;
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require('cssnano');
+
+module.exports = {
+    plugins: [
+        require("postcss-import")(),
+        require("tailwindcss"),
+        require("autoprefixer"),
+        cssnano({
+         autoprefixer: false,
+         preset: ['default'],
+	    }),
+		// Only purge css on production
+        production &&
+            purgecss({
+                content: ["./public/**/*.html", "./src/**/*.svelte"],
+                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g || [])
+            })
+    ]
+}
